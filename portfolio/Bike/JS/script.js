c908342c7f241860fbf60beb65ef715b6ff1d1e9
AlexHalin <72556698+AlexHalin@users.noreply.github.com>
2021-03-20 20:31:34 +0200

    add links in portfolio

--- portfolio/Bike/JS/script.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".burger");
-const navLinks = document.querySelector(".site_navigation");
-const body = document.querySelector("body")
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  hamburger.classList.toggle("open_burger")
-  body.classList.toggle("lock")
-});
